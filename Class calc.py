59280ae2d15abf22e261e77837188c48377856fc
Tanushka27 <92003667+Tanushka27@users.noreply.github.com>
2021-10-06 07:52:18 +0530

    Add files via upload

---
+++ Class calc.py
@@ -0,0 +1,15 @@
+print("Calculator(Addition,Subtraction,Multiplication and Division)")
+No1=input("Enter First value:")
+No1= eval(No1)
+No2= input("Enter Second value:")
+No2= eval(No2)
+Sum= No1+No2
+print (" Sum of both the values=",Sum)
+Diff= No1-No2
+print("Difference of both values=",Diff)
+prod= No1*No2
+print("Product of both the values=",prod)
+Div= No1/No2
+print ("Division of the value is=",Div)
+mod= No1%No2
+print ("remainder is=",mod)
